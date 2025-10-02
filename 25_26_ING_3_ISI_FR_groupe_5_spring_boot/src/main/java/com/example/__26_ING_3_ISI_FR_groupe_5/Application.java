60deb79c0434c49fb218930f034546c488562d0d
JUNIOR NOUMEDEM <juniornoumedem02@gmail.com>
2025-10-02 03:10:39 +0100

    Initialisation du projet spring boot 25_26_ING_FR_groupe_5

---
+++ 25_26_ING_3_ISI_FR_groupe_5_spring_boot/src/main/java/com/example/__26_ING_3_ISI_FR_groupe_5/Application.java
@@ -0,0 +1,13 @@
+package com.example.__26_ING_3_ISI_FR_groupe_5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ 25_26_ING_3_ISI_FR_groupe_5_spring_boot/src/test/java/com/example/__26_ING_3_ISI_FR_groupe_5/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.__26_ING_3_ISI_FR_groupe_5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
